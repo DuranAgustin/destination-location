ba5785b9014e3e8f27674c6cc97a01bd5cb25bc9
Agustin <agustinduranse@gmail.com>
2022-07-17 18:59:21 -0700

    edit and delete buttons

--- outputdis.js
+++ outputdis.js
@@ -6,12 +6,9 @@ document.getElementById("submit_btn").addEventListener("click", (evtObj) => {
   let location = document.getElementById("location").value;
   let photo = document.getElementById("photo").value;
   let description = document.getElementById("description").value;
-  //   console.log(destination);
-  //   console.log(location);
-  //   console.log(photo);
-  //   console.log(description);
-  let newCard = createCard(destination, location, photo, description);
-  document.getElementById("card_container").appendChild(newCard);
+
+  let setter = createCard(destination, location, photo, description);
+  document.getElementById("card_container").appendChild(setter);
 
   document.getElementById("input").reset();
 });
@@ -26,14 +23,15 @@ function createCard(destination, location, photo, description) {
 
   var img = document.createElement("img");
   img.setAttribute("class", "img");
+  img.setAttribute("alt", destination);
   let photoURL =
     "https://images.unsplash.com/photo-1473442240418-452f03b7ae40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZGVzdGluYXRpb258ZW58MHx8MHx8&w=1000&q=80";
   if (photo.length === 0) {
     img.src = photoURL;
-    console.log("set");
+    //console.log("set");
   } else {
     img.src = photo;
-    console.log("set12");
+    //console.log("set2");
   }
   card.appendChild(img);
 
@@ -47,5 +45,29 @@ function createCard(destination, location, photo, description) {
   cardDis.innerText = description;
   card.appendChild(cardDis);
 
+  let editButton = document.createElement("button");
+  editButton.setAttribute("class", "buttonEdit");
+  editButton.innerText = "Edit";
+  editButton.addEventListener("click", editCard);
+  card.appendChild(editButton);
+
+  let deleteButton = document.createElement("button");
+  deleteButton.setAttribute("class", "buttonDelete");
+  deleteButton.innerText = "delete";
+  deleteButton.addEventListener("click", deleteCard);
+  card.appendChild(deleteButton);
+
   return card;
+}
+
+function editCard(event) {
+  let card = event.target.parentElement;
+  card.children[0].innerText = window.prompt("enter new name");
+  card.children[1].innerText = window.prompt("enter new location");
+  card.children[2].innerText = window.prompt("enter new discription");
+}
+
+function deleteCard(event) {
+  let card = event.target.parentElement;
+  card.remove();
 }
